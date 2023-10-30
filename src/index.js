512f098abccd20f27faa8fdc68fb364c24b2494b
Tim-PF <tim.pfops@gmail.com>
2023-10-30 18:46:12 +0100

    Added form to sidebar

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import './style.css';
 import _ from 'lodash';
 import {toggleSideBar} from './sidebar';
 import {toggleTheme} from './theme';
+import {projectButtonClicked} from './new-project'
 
 
 
 home();
 toggleSideBar();
 toggleTheme();
+projectButtonClicked();
 
   function home() {
     const home = document.querySelector('#main');
@@ -18,7 +20,19 @@ toggleTheme();
         <button id="theme-button">Toggle MODE</button>      
         </div>      
       <div class="content">
-        <div class="left-panel"></div>
+        <div class="left-panel">
+          <h1>Home</h1>
+          <div class="Projects">
+            <h1>Projects</h1>
+            <form id="projectForm" class="hidden">
+                <div class="projectIcon">
+                            <span class="material-icons-round">menu</span>
+                </div>
+                <input type="text" id="fname" name="fname"><br>
+            </form>
+            <button id="addProject"><span class="material-icons-round">add_circle_outlined</span>Add Project</button>
+          </div>
+        </div>
         <div class="right-panel"></div>
       </div>
 
