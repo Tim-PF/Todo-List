c40c02243a9140cd31009fccec3b0459c48149a4
Tim-PF <tim.pfops@gmail.com>
2023-10-31 10:18:49 +0100

    Functions to create and print Project Objects

--- src/createProject.js
+++ src/createProject.js
@@ -5,3 +5,42 @@ class Project {
     }
 }
 
+const projectList = [];
+
+export function createProject(prName) {
+  let projectName = new Project(prName) ;
+  projectList.push(projectName);
+  printForm()
+}
+
+//Prints all Projects inside projectlist to the sidebar
+function printForm() {
+   let projectData = 0;
+
+   const sidebarProjects = document.querySelector('.Projects');
+
+   
+   deleteCurrentProjects(sidebarProjects)
+   
+
+    for (const project of projectList) {
+    
+
+       let  projectDiv = document.createElement('div')
+       projectDiv.textContent = project.name;
+       projectDiv.classList.add('project-item')
+       projectDiv.setAttribute('data-project', projectData);
+
+     
+       sidebarProjects.appendChild(projectDiv)
+
+       projectData++;
+
+    }
+}
+
+//Prevents Divs from getting printed more than one time 
+function deleteCurrentProjects(sidebarProjects) {
+    const existingProjectItems = sidebarProjects.querySelectorAll('.project-item');
+    existingProjectItems.forEach(item => item.remove());
+}
